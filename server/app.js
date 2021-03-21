2e9251ea2477d341b3c38b119827fd57d97fc8ad
wibowo-laks <laksana.wibowo93@gmail.com>
2021-03-21 13:55:46 +0700

    create permintaan toko

--- server/app.js
+++ server/app.js
@@ -4,13 +4,14 @@ const { connect } = require('./config/mongodb')
 const purchasingOrderSchema = require('./schemas/purchasingOrder')
 const adminSchema = require('./schemas/admin')
 const checkerSchema = require('./schemas/checker')
+const storeSchema = require('./schemas/storage_child')
 
 const customScalarResolver = require('./schemas/graphQlResolver')
 
 const server = new ApolloServer({
-    typeDefs: [Schemas.typeDefs, purchasingOrderSchema.typeDefs, adminSchema.typeDefs, checkerSchema.typeDefs],
-    resolvers: [customScalarResolver, Schemas.resolvers, adminSchema.resolvers, purchasingOrderSchema.resolvers, checkerSchema.resolvers]
-})
+  typeDefs: [Schemas.typeDefs, purchasingOrderSchema.typeDefs, adminSchema.typeDefs, checkerSchema.typeDefs, storeSchema.typeDefs],
+  resolvers: [customScalarResolver, Schemas.resolvers, adminSchema.resolvers, purchasingOrderSchema.resolvers, checkerSchema.resolvers, storeSchema.resolvers],
+});
 
 connect().then(async (db) => {
     console.log('database connected')
---
+++ server/schemas/storage_child.js
@@ -0,0 +1,70 @@
+const { gql, ApolloError } = require("apollo-server");
+const StorageChild = require("../models/storage_child");
+
+module.exports = {
+  typeDefs: gql`
+    # scalar Date
+
+    type StorageChild {
+      _id: ID!
+      name: String
+      item: [reqItem]
+      createdAt: Date
+      updatedAt: Date
+    }
+
+    type reqItem {
+      name: String
+      quantity: Int
+    }
+
+    input inputItem {
+      name: String
+      quantity: Int
+    }
+
+    input CreateStorageChild {
+      name: String
+      item: [inputItem]
+    }
+
+    extend type Query {
+      stores: [StorageChild]
+    }
+
+    extend type Mutation {
+      createStore(store: CreateStorageChild): StorageChild
+    }
+  `,
+
+  resolvers: {
+    Query: {
+      stores: async () => {
+        try {
+          const respone = await StorageChild.findAll();
+          return respone;
+        } catch (error) {
+          console.log(error, "---> error");
+          return new ApolloError(error);
+        }
+      },
+    },
+
+    Mutation: {
+      createStore: async (_, args) => {
+        try {
+          let dataInput = { ...args.store };
+          console.log("masuk");
+          console.log(args.store);
+          dataInput.createdAt = new Date();
+          dataInput.updatedAt = new Date();
+          const respone = await StorageChild.create(dataInput);
+          return respone.ops[0];
+        } catch (error) {
+          console.log(error, "---> error");
+          return new ApolloError(error);
+        }
+      },
+    },
+  },
+};
